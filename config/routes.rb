00477b9a62e6fb7a48d77db5cb9252acc369a5f7
Yuki Yamamoto <yamanoieyamanoie@yahoo.co.jp>
2020-07-06 11:57:02 +0000

    about view finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root "homes#top"
+  get "about" => "homes#about"
+  devise_for :users
+  resource :users
+  resource :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
