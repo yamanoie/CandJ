d0d34ff0ca6db5d551323094361ee44906c61a1f
Yuki Yamamoto <yamanoieyamanoie@yahoo.co.jp>
2020-07-08 10:18:15 +0000

    contoroller blogs

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,28 @@
+class BlogsController < ApplicationController
+  def index
+  	@blogs = Blog.all
+  end
+
+  def show
+  	@blog = Blog.find(params[:id])
+  end
+  def new
+  	@blog = Blog.new
+  end
+  def create
+  	blog = Blog.new(blog_params)
+  	blog.save
+  	redirect_to blogs_path
+  end
+  def edit
+  end
+  def update
+  end
+  def destroy
+  end
+
+private
+  def blog_params
+  	params.require(:blog).permit(:title, :body)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get "about" => "homes#about"
   devise_for :users
   resource :users
-  resource :posts
+  resource :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
